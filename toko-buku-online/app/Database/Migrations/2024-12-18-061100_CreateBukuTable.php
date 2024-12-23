f325575572db233935a8d60d94ce72325c10ea9a
LORDRISKI <riskisaputra9726@gmail.com>
2024-12-23 14:30:19 +0700

    Toko Buku Online

--- toko-buku-online/app/Database/Migrations/2024-12-18-061100_CreateBukuTable.php
+++ toko-buku-online/app/Database/Migrations/2024-12-18-061100_CreateBukuTable.php
@@ -8,34 +8,42 @@ class CreateBukuTable extends Migration
 {
     public function up()
     {
-        $this->forge->addField([
-            'id' => [
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-                'auto_increment' => true,
-            ],
-            'judul' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 255,
-            ],
-            'pengarang' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 255,
-            ],
-            'tahun' => [
-                'type'           => 'INT',
-                'constraint'     => 5,
-            ],
-            'THUMBNAIL_URL' => [
-                'type'           => 'VARCHAR',
-                'constraint'     => 250,
+        $this->forge->addField(
+            [
+                'id' => [
+                    'type' => 'INT',
+                                'constraint' => 5,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                ],
+                'judul' => [
+                    'type'      => 'VARCHAR',
+                    'constraint'=> 250
+                ],
+                'pengarang' => [
+                    'type'      => 'VARCHAR',
+                    'constraint'=> 250
+                ],
+                'penerbit' => [
+                    'type'      => 'VARCHAR',
+                    'constraint'=> 250
+                ],
+                'tahun' => [
+                    'type'      => 'INT',
+                    'constraint'=> 5
+                ],
+                'thumbnail_url' => [
+                    'type'      => 'VARCHAR',
+                    'constraint'=> 250
+                ]
             ]
-            ]);
+        );
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('books');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('books');
     }
 }
